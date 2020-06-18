56c36602b5c913f10f2afd36f9edb6781e5a8aff
Felix Hägglund Wennergren <felixhow@icloud.com>
2020-06-18 02:28:22 +0200

    config: Update

--- config.def.h
+++ config.def.h
@@ -3,7 +3,7 @@
 
 #define MOD Mod4Mask
 
-const char* menu[]    = {"dmenu_run",               0};
+const char* menu[]    = {"dmn",                     0};
 const char* term[]    = {"st",                      0};
 const char* scrot[]   = {"scr",                     0};
 const char* briup[]   = {"xbacklight", "-inc", "5", 0};
@@ -56,12 +56,18 @@ static struct key keys[] = {
     {MOD|ShiftMask, XK_2, win_to_ws, {.i = 2}},
     {MOD,           XK_3, ws_go,     {.i = 3}},
     {MOD|ShiftMask, XK_3, win_to_ws, {.i = 3}},
-    {MOD,           XK_8, ws_go,     {.i = 4}},
-    {MOD|ShiftMask, XK_8, win_to_ws, {.i = 4}},
-    {MOD,           XK_9, ws_go,     {.i = 5}},
-    {MOD|ShiftMask, XK_9, win_to_ws, {.i = 5}},
-    {MOD,           XK_0, ws_go,     {.i = 6}},
-    {MOD|ShiftMask, XK_0, win_to_ws, {.i = 6}},
+    {MOD,           XK_4, ws_go,     {.i = 4}},
+    {MOD|ShiftMask, XK_4, win_to_ws, {.i = 4}},
+    {MOD,           XK_5, ws_go,     {.i = 5}},
+    {MOD|ShiftMask, XK_5, win_to_ws, {.i = 5}},
+    {MOD,           XK_6, ws_go,     {.i = 6}},
+    {MOD|ShiftMask, XK_6, win_to_ws, {.i = 6}},
+    {MOD,           XK_7, ws_go,     {.i = 7}},
+    {MOD|ShiftMask, XK_7, win_to_ws, {.i = 7}},
+    {MOD,           XK_8, ws_go,     {.i = 8}},
+    {MOD|ShiftMask, XK_8, win_to_ws, {.i = 8}},
+    {MOD,           XK_9, ws_go,     {.i = 9}},
+    {MOD|ShiftMask, XK_9, win_to_ws, {.i = 9}},
 };
 
 #endif
