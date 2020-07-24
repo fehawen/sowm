5e3e149690eeb57bfa2e53a25318b6639d166cab
Felix Hägglund Wennergren <felixhow@icloud.com>
2020-07-24 15:47:11 +0200

    config: Add script keybinding triggers

--- config.def.h
+++ config.def.h
@@ -7,6 +7,10 @@
 const char* menu[]    = {"dmn",                     0};
 const char* term[]    = {"st",                      0};
 const char* scrot[]   = {"scr",                     0};
+const char* clock[]   = {"clk",                     0};
+const char* battery[] = {"bat", "notify",           0};
+const char* light[]   = {"rpg", "light",            0};
+const char* dark[]    = {"rpg", "dark",             0};
 const char* briup[]   = {"xbacklight", "-inc", "5", 0};
 const char* bridown[] = {"xbacklight", "-dec", "5", 0};
 const char* voldown[] = {"pamixer", "-d", "5",      0};
@@ -41,9 +45,13 @@ static struct key keys[] = {
     {MOD,           XK_Tab, win_next,   {0}},
     {MOD|ShiftMask, XK_Tab, win_prev,   {0}},
 
-    {MOD, XK_d,      run, {.com = menu}},
-    {MOD, XK_p,      run, {.com = scrot}},
-    {MOD, XK_Return, run, {.com = term}},
+    {MOD, XK_d,           run, {.com = menu}},
+    {MOD, XK_p,           run, {.com = scrot}},
+    {MOD, XK_Return,      run, {.com = term}},
+    {MOD, XK_t,           run, {.com = clock}},
+    {MOD, XK_b,           run, {.com = battery}},
+    {MOD|ShiftMask, XK_a, run, {.com = light}},
+    {MOD|ShiftMask, XK_d, run, {.com = dark}},
 
     {0,   XF86XK_AudioLowerVolume,  run, {.com = voldown}},
     {0,   XF86XK_AudioRaiseVolume,  run, {.com = volup}},
