858bb9da93aefa4b34272a66b2e6d092cb5a4a28
Felix Hägglund Wennergren <felixhow@icloud.com>
2020-08-10 14:21:07 +0200

    config: Increase corner radius

--- config.def.h
+++ config.def.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 #define MOD Mod4Mask
-#define ROUND_CORNERS 12
+#define ROUND_CORNERS 18
 
 const char* menu[]    = {"dmn",                     0};
 const char* term[]    = {"st",                      0};
