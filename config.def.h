5c43e97ac180255c451f165d6359ca1c685bf9a8
Felix Hägglund Wennergren <felixhow@icloud.com>
2020-09-01 17:55:16 +0200

    config.def.h: Update

--- config.def.h
+++ config.def.h
@@ -4,12 +4,11 @@
 #define MOD Mod4Mask
 
 const char* menu[]    = {"dmn",                     0};
-const char* term[]    = {"st",                      0};
+const char* term[]    = {"xterm",                   0};
 const char* scrot[]   = {"scr",                     0};
 const char* clock[]   = {"clk",                     0};
 const char* battery[] = {"bat", "notify",           0};
-const char* light[]   = {"rpg", "light",            0};
-const char* dark[]    = {"rpg", "dark",             0};
+const char* palette[] = {"rpg", "g",                0};
 const char* briup[]   = {"xbacklight", "-inc", "5", 0};
 const char* bridown[] = {"xbacklight", "-dec", "5", 0};
 const char* voldown[] = {"pamixer", "-d", "5",      0};
@@ -17,10 +16,10 @@ const char* volup[]   = {"pamixer", "-i", "5",      0};
 const char* volmute[] = {"pamixer", "-t",           0};
 
 static struct key keys[] = {
-    {MOD,  XK_q,  win_kill,   {0}},
-    {MOD,  XK_c,  win_center, {0}},
-    {MOD,  XK_f,  win_fs,     {0}},
-    {MOD,  XK_m,  win_mx,     {0}},
+    {MOD|ShiftMask,    XK_q,  win_kill,   {0}},
+    {MOD,              XK_c,  win_center, {0}},
+    {MOD,              XK_f,  win_fs,     {0}},
+    {MOD,              XK_m,  win_mx,     {0}},
 
     {MOD,              XK_k,  win_move,  {.com = (const char*[]){"move",   "n"}, .i = 100}},
     {MOD,              XK_j,  win_move,  {.com = (const char*[]){"move",   "s"}, .i = 100}},
@@ -50,8 +49,7 @@ static struct key keys[] = {
     {MOD, XK_Return,      run, {.com = term}},
     {MOD, XK_t,           run, {.com = clock}},
     {MOD, XK_b,           run, {.com = battery}},
-    {MOD|ShiftMask, XK_a, run, {.com = light}},
-    {MOD|ShiftMask, XK_d, run, {.com = dark}},
+    {MOD|ShiftMask, XK_d, run, {.com = palette}},
 
     {0,   XF86XK_AudioLowerVolume,  run, {.com = voldown}},
     {0,   XF86XK_AudioRaiseVolume,  run, {.com = volup}},
